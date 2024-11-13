b758ee8ce1375cf8d31ad58d548f699d1d1aae3e
Davis Hoang <davishoang96@gmail.com>
2024-11-13 16:46:27 +0800

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,6 +29,10 @@ const TaskManagement = () => {
 		);
 	};
 
+	const handleTaskDelete = (taskId: number) => {
+		setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+	};
+
 	const deleteAllTasks = async () => {
 		const response = await fetch("/api/deleteAllTasks", {
 			method: "DELETE",
@@ -44,7 +48,7 @@ const TaskManagement = () => {
 	return (
 		<div className="container mt-5">
 			<h1>Task Management</h1>
-			<ListAllTask tasks={tasks} onTaskUpdate={updateTask} />
+			<ListAllTask tasks={tasks} onTaskUpdate={updateTask} onTaskDelete={handleTaskDelete} />
 			<CreateTaskForm addTask={addTask} />
 			<button className="btn btn-danger mt-3" onClick={deleteAllTasks}>
 				Purge Database
